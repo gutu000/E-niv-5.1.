b085f6c9eefd0cd67a8851f7f659bd82681bc170
Pingvu <nhlpool27@gmail.com>
2023-01-26 13:24:54 +0100

    Add project files.

---
+++ E-nivå 5.1/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] namn = new string[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Stad:");
+    namn[i] = Console.ReadLine();
+}
+Console.WriteLine($"Du skrev in{namn[0]}, {namn[1]}, {namn[2]}, {namn[3]}, {namn[4]}");
